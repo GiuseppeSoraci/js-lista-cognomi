6523519736ed0daba10a5d237de903dc07e9fbde
GiuseppeSoraci <78480251+GiuseppeSoraci@users.noreply.github.com>
2021-03-24 20:54:15 +0100

    js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Prompt
+var surnameUser = prompt("What's your surname?").toLowerCase().trim();
+
+// Array
+var list = [
+    "bianchi", 
+    "neri", 
+    "rossi", 
+    "verdi", 
+    "gialli"
+];
+
+list.push(surnameUser);
+list.sort();
+
+// User position
+var position = (list.indexOf(surnameUser) + 1);
+
+// Loop
+var surnames = " ";
+for (var i = 0; i < list.length; i++) {
+    surnames += "<li>" + list[i] + "</li>";
+}
+
+// Output 
+document.getElementById("list").innerHTML = surnames;
+document.getElementById("position").innerHTML = "Surname you entered is in position number " + position;
